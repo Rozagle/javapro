4da3fb34016438ba78fdb15b50591985c3d4e3fd
Rozagle <149897112+Rozagle@users.noreply.github.com>
2023-12-22 11:58:19 +0300

    Update TaksimetreProgrami.java

--- TaksimetreProgrami.java
+++ TaksimetreProgrami.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HelloWorld {
+public class TaksimetreProgrami  {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Gidilen Mesafe (KM): ");
