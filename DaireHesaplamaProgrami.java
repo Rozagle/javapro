76054a4ce67c24e5b5bd75ae811c13727096f5d9
Rozagle <149897112+Rozagle@users.noreply.github.com>
2023-12-22 11:56:15 +0300

    Create DaireHesaplamaProgrami.java

---
+++ DaireHesaplamaProgrami.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    
+        System.out.print("Dairenin Yari capini Girin: ");
+        double yariCap = scanner.nextDouble();
+        final double pi = 3.14;
+
+        
+        double alan = pi * yariCap * yariCap;
+        System.out.println("Dairenin Alani: " + alan);
+
+
+        double cevre = 2 * pi * yariCap;
+        System.out.println("Dairenin cevresi: " + cevre);
+
+        scanner.close();
+    }
+}
