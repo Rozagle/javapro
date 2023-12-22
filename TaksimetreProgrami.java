09f6a379d33417516ec13c978eeb40fddec8f9ab
Rozagle <149897112+Rozagle@users.noreply.github.com>
2023-12-22 11:50:35 +0300

    Create TaksimetreProgrami.java

---
+++ TaksimetreProgrami.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Gidilen Mesafe (KM): ");
+        double mesafe = scanner.nextDouble();
+        double BaslangicUcreti = 10.0;  
+        double kmBasinaTutar = 2.20; 
+        double minOdeme = 20.0; 
+        double taksimetreTutar = BaslangicUcreti + (mesafe * kmBasinaTutar);
+        if (taksimetreTutar < minOdeme) {
+           
+             System.out.println("Taksimetre Tutari: " + minOdeme + " TL");
+        }else {
+             System.out.println("Taksimetre Tutari: " + taksimetreTutar + " TL");
+        }
+        scanner.close();
+    }
+}
